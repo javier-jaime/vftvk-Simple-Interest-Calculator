7efe30270b3a5f4a04f5e0ff6ddfeb2623833c68
javier-jaime <68567459+javier-jaime@users.noreply.github.com>
2021-05-27 14:46:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+// Function that Compute the Interest
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    // Principal input box Validation
+    if (principal <= 0 || isNaN(principal))
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    // Interest Calculation
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <strong>$"+principal+"</strong>,\<br\>at an interest rate of <strong>"+rate+"%</strong>\<br\>You will receive an amount of <strong>$"+interest+"</strong>,\<br\>in the year <strong>"+year+"</strong>\<br\>"    
 }
-        
+// Function that reads the value of the range slider
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
